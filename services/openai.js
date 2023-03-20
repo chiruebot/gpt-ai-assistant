e577cbca0abcbb230ab0444d18f45f0d3a4d297c
ChiBot <128030790+chiruebot@users.noreply.github.com>
2023-03-20 14:25:27 +0800

    Update openai.js

--- services/openai.js
+++ services/openai.js
@@ -14,92 +14,92 @@ export const IMAGE_SIZE_256 = '256x256';
 export const IMAGE_SIZE_512 = '512x512';
 export const IMAGE_SIZE_1024 = '1024x1024';
 
-export const MODEL_GPT_3_5_TURBO = 'text-davinci-002';
-export const MODEL_WHISPER_1 = 'speech-davinci-002';
+export const MODEL_GPT_3_5_TURBO = 'gpt-3.5-turbo';
+export const MODEL_WHISPER_1 = 'whisper-1';
 
 const client = axios.create({
-baseURL: 'https://api.openai.azure.com',
-timeout: config.OPENAI_TIMEOUT,
-headers: {
-'Accept-Encoding': 'gzip, deflate, compress',
-},
+  baseURL: 'https://api.openai.com',
+  timeout: config.OPENAI_TIMEOUT,
+  headers: {
+    'Accept-Encoding': 'gzip, deflate, compress',
+  },
 });
 
 client.interceptors.request.use((c) => {
-c.headers.Authorization = Bearer ${config.AZURE_OPENAI_API_KEY};
-return handleRequest(c);
+  c.headers.Authorization = `Bearer ${config.OPENAI_API_KEY}`;
+  return handleRequest(c);
 });
 
 client.interceptors.response.use(handleFulfilled, (err) => {
-if (err.response?.data?.error?.message) {
-err.message = err.response.data.error.message;
-}
-return handleRejected(err);
+  if (err.response?.data?.error?.message) {
+    err.message = err.response.data.error.message;
+  }
+  return handleRejected(err);
 });
 
 const createChatCompletion = ({
-model = config.AZURE_OPENAI_COMPLETION_MODEL,
-messages,
-temperature = config.AZURE_OPENAI_COMPLETION_TEMPERATURE,
-maxTokens = config.AZURE_OPENAI_COMPLETION_MAX_TOKENS,
-frequencyPenalty = config.AZURE_OPENAI_COMPLETION_FREQUENCY_PENALTY,
-presencePenalty = config.AZURE_OPENAI_COMPLETION_PRESENCE_PENALTY,
+  model = config.OPENAI_COMPLETION_MODEL,
+  messages,
+  temperature = config.OPENAI_COMPLETION_TEMPERATURE,
+  maxTokens = config.OPENAI_COMPLETION_MAX_TOKENS,
+  frequencyPenalty = config.OPENAI_COMPLETION_FREQUENCY_PENALTY,
+  presencePenalty = config.OPENAI_COMPLETION_PRESENCE_PENALTY,
 }) => client.post('/v1/chat/completions', {
-model,
-messages,
-temperature,
-max_tokens: maxTokens,
-frequency_penalty: frequencyPenalty,
-presence_penalty: presencePenalty,
+  model,
+  messages,
+  temperature,
+  max_tokens: maxTokens,
+  frequency_penalty: frequencyPenalty,
+  presence_penalty: presencePenalty,
 });
 
 const createTextCompletion = ({
-model = config.AZURE_OPENAI_COMPLETION_MODEL,
-prompt,
-temperature = config.AZURE_OPENAI_COMPLETION_TEMPERATURE,
-maxTokens = config.AZURE_OPENAI_COMPLETION_MAX_TOKENS,
-frequencyPenalty = config.AZURE_OPENAI_COMPLETION_FREQUENCY_PENALTY,
-presencePenalty = config.AZURE_OPENAI_COMPLETION_PRESENCE_PENALTY,
-stop = [
-${ROLE_AI}:,
-${ROLE_HUMAN}:,
-],
+  model = config.OPENAI_COMPLETION_MODEL,
+  prompt,
+  temperature = config.OPENAI_COMPLETION_TEMPERATURE,
+  maxTokens = config.OPENAI_COMPLETION_MAX_TOKENS,
+  frequencyPenalty = config.OPENAI_COMPLETION_FREQUENCY_PENALTY,
+  presencePenalty = config.OPENAI_COMPLETION_PRESENCE_PENALTY,
+  stop = [
+    ` ${ROLE_AI}:`,
+    ` ${ROLE_HUMAN}:`,
+  ],
 }) => client.post('/v1/completions', {
-model,
-prompt,
-temperature,
-max_tokens: maxTokens,
-frequency_penalty: frequencyPenalty,
-presence_penalty: presencePenalty,
-stop,
+  model,
+  prompt,
+  temperature,
+  max_tokens: maxTokens,
+  frequency_penalty: frequencyPenalty,
+  presence_penalty: presencePenalty,
+  stop,
 });
 
 const createImage = ({
-prompt,
-n = 1,
-size = IMAGE_SIZE_256,
+  prompt,
+  n = 1,
+  size = IMAGE_SIZE_256,
 }) => client.post('/v1/images/generations', {
-prompt,
-n,
-size,
+  prompt,
+  n,
+  size,
 });
 
 const createAudioTranscriptions = ({
-buffer,
-file,
-model = MODEL_WHISPER_1,
+  buffer,
+  file,
+  model = MODEL_WHISPER_1,
 }) => {
-const formData = new FormData();
-formData.append('file', buffer, file);
-formData.append('model', model);
-return client.post('/v1/audio/transcriptions', formData.getBuffer(), {
-headers: formData.getHeaders(),
-});
+  const formData = new FormData();
+  formData.append('file', buffer, file);
+  formData.append('model', model);
+  return client.post('/v1/audio/transcriptions', formData.getBuffer(), {
+    headers: formData.getHeaders(),
+  });
 };
 
 export {
-createChatCompletion,
-createTextCompletion,
-createImage,
-createAudioTranscriptions,
+  createChatCompletion,
+  createTextCompletion,
+  createImage,
+  createAudioTranscriptions,
 };
